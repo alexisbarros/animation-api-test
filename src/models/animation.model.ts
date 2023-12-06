5bf172ee3ed2cf3749550a6d2e2a1806eae1a30b
Alexis Barros <apenasalexis@gmail.com>
2023-12-06 17:02:37 -0300

    :bug: add _id and fix date type

--- src/models/animation.model.ts
+++ src/models/animation.model.ts
@@ -1,12 +1,18 @@
-
-import * as _model from '.';
-import * as _interface from '../interfaces';
-
 export class Animation {
-  name: String;description?: String;startDate?: Number;finishDate?: Number;_deletedAt?: Number;
-  
+  _id?: String;
+  name: String;
+  description?: String;
+  startDate?: Number | String;
+  finishDate?: Number | String;
+  _deletedAt?: Number;
+
   constructor(data: any) {
-    this.name = data.name;this.description = data.description;this.startDate = data.startDate;this.finishDate = data.finishDate;this._deletedAt = data._deletedAt;
+    this._id = data._id;
+    this.name = data.name;
+    this.description = data.description;
+    this.startDate = data.startDate && new Date(data.startDate).toDateString();
+    this.finishDate =
+      data.startDate && new Date(data.finishDate).toDateString();
+    this._deletedAt = data._deletedAt;
   }
 }
-  
--- src/models/character.model.ts
+++ src/models/character.model.ts
@@ -1,13 +1,23 @@
-
 import * as _model from '.';
 import * as _interface from '../interfaces';
 
 export class Character {
-  name?: String;sex?: String;characterType?: String;animationId?: _interface.IAnimation | String;_deletedAt?: Number;
-  
+  _id?: String;
+  name?: String;
+  sex?: String;
+  characterType?: String;
+  animationId?: _interface.IAnimation | String;
+  _deletedAt?: Number;
+
   constructor(data: any) {
-    this.name = data.name;this.sex = data.sex;this.characterType = data.characterType;this.animationId = data.animationId && typeof data.animationId === 'object' ? 
-        new _model.Animation(data.animationId) : data.animationId;this._deletedAt = data._deletedAt;
+    this._id = data._id;
+    this.name = data.name;
+    this.sex = data.sex;
+    this.characterType = data.characterType;
+    this.animationId =
+      data.animationId && typeof data.animationId === 'object'
+        ? new _model.Animation(data.animationId)
+        : data.animationId;
+    this._deletedAt = data._deletedAt;
   }
 }
-  
